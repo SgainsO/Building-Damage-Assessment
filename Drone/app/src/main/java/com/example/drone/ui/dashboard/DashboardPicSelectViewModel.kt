361691f6c69aa227ed8c640fec2f45f355785234
SgainsO <jptjasc@gmail.com>
2025-05-22 19:40:16 -0400

    Make the app more better

--- Drone/app/src/main/java/com/example/drone/ui/dashboard/DashboardPicSelectViewModel.kt
+++ Drone/app/src/main/java/com/example/drone/ui/dashboard/DashboardPicSelectViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import android.widget.ImageView
 import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicInteger
+
 
 class DashboardPicSelectViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -16,24 +18,27 @@ class DashboardPicSelectViewModel(application: Application) : AndroidViewModel(a
     }
     val database = AppDatabase.getDatabase(application)
 
+    val currentPinNumber = AtomicInteger(0)
+
+    fun resetPinNumber()
+    {
+        viewModelScope.launch {
+            picId = database.picturesDao().getIdForName(pictureName) ?: -1
+        }
+    }
+
     init {
-        resetPinNumber()
     }
 
     // https://medium.com/@harimoradiya123/getting-started-with-room-database-in-android-using-kotlin-92f84b6a5e6c
-
-    var currentPinNumber = 0
 
     var IdLastInServer: Int? = null
 
     val text: LiveData<String> = _text
 
-    fun resetPinNumber()
-    {
-        viewModelScope.launch {
-            currentPinNumber = database.selectedDao().GetMaxPin(-1) + 1
-        }
-    }
+    var picId: Int = -1
+    //To Do, make a function that will allow the frontend to populate this value at stand by
+
     private var LocationData = mutableMapOf("imageChord" to listOf(0f,0f), "chord" to listOf(0f,0f))
     val LocationInfoArray : MutableList<MutableMap<String, List<Float>>> = mutableListOf()
 
@@ -42,14 +47,13 @@ class DashboardPicSelectViewModel(application: Application) : AndroidViewModel(a
 
     fun SaveClicked(x_clicked: Float, y_clicked:Float)
     {
-        currentPinNumber += 1
         Log.d("INPUT","Tap On: ($x_clicked, $y_clicked)")
         LocationData["imageChord"] = listOf(x_clicked, y_clicked)
 
         viewModelScope.launch {
-            Log.d("pointID", "$currentPinNumber")
+            Log.d("pointID", "${currentPinNumber.get()}")
             val newPictureIntoData = PictureInfo(
-                currentPinNumber,
+                database.selectedDao().GetMaxPin(-1) + 1,
                 database.picturesDao().getIdForName(pictureName) ?: -1,
                 x_clicked,
                 y_clicked,
@@ -68,33 +72,43 @@ class DashboardPicSelectViewModel(application: Application) : AndroidViewModel(a
         LocationData["chord"] = listOf(x, y)
         LocationInfoArray.add(LocationData)
         LocationData = mutableMapOf("imageChord" to listOf(0f,0f), "chord" to listOf(0f,0f))
-
+        val pinNum = currentPinNumber.get()
         Log.d("before", "make")
 
         viewModelScope.launch {
-            val picId = database.picturesDao().getIdForName(pictureName) ?: -1
-            Log.d("THREAD", "picID: $picId pointID: $currentPinNumber")
-            val halfLocation = database.selectedDao().GetPinLocation(picId, currentPinNumber - 1)
+            Log.d("THREAD", "picID: $picId pointID: ${pinNum}")
+
+            val halfLocation = database.selectedDao().GetPinLocation(picId,
+                database.selectedDao().GetMaxPin(-1))
 
             halfLocation.let {
                 it.chordX = x
                 it.chordY = y
                 database.selectedDao().update(it)
             }
+            // + 1
         }
-            //The thing will crash
-    //TO DO: Send to a database
+        //The thing will crash
+        //TO DO: Send to a database
     }
 
     fun cancelPinPressed()
     {   //For when the cancel button is being pressed
-       // currentPinNumber -= 1
+        viewModelScope.launch {
+            database.selectedDao().delete(
+                database.selectedDao().GetPinLocation(
+                    picId,
+                    database.selectedDao().GetMaxPin(-1)
+                )
+            )
+
+        }
     }
 
-    fun editTextReflectTrueName(picID: Int)
+    fun editTextReflectTrueName(piczID: Int)
     {
         viewModelScope.launch {
-            _text.postValue( database.picturesDao().getNameForId(picID) ?: "empty")
+            _text.postValue( database.picturesDao().getNameForId(piczID) ?: "empty")
         }
     }
 
