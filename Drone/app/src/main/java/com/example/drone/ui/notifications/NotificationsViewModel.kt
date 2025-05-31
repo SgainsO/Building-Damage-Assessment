517f26910678c6c57066e8cf4a73a570f3ad380a
sgainso <jptjasc@gmail.com>
2025-05-30 23:03:02 -0400

    Reworked NAV Bar to Match an expected Drone UI

--- Drone/app/src/main/java/com/example/drone/ui/notifications/NotificationsViewModel.kt
+++ Drone/app/src/main/java/com/example/drone/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Empty Drones Container"
     }
     val text: LiveData<String> = _text
 }
